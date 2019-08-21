74d040dc4b26be7bac53d931f95fdc08aa7363e8
Kia Ishii <kia.king.08@gmail.com>
2019-08-21 18:23:07 +0900

    Add basic foundation for stylings

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="App">
-    <Nuxt />
+    <div class="content">
+      <Nuxt />
+    </div>
   </div>
 </template>
+
+<style lang="postcss" scoped>
+.App {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.content {
+  flex-grow: 1;
+}
+</style>
