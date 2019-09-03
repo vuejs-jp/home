c0f813e02362822ab71f8ad721a6794563ed98fe
Kia King Ishii <kia.king.08@gmail.com>
2019-09-03 23:27:36 +0900

    Add Code of Conduct page

---
+++ src/components/AppPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="AppPage">
+    <slot />
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+.AppPage.page-leave-active {
+  transition: opacity 0.1s, transform 0.1s;
+}
+
+.AppPage.page-leave-to {
+  opacity: 0;
+  transform: translateY(-4px);
+}
+</style>
