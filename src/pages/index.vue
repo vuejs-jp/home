d8ce3f607d891527f29f8e00aebca12a98daffc4
Kia Ishii <kia.king.08@gmail.com>
2019-09-06 16:09:49 +0900

    Add Home Hero section

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,19 +1,26 @@
 <template>
-  <AppPage class="Home" />
+  <AppPage class="Home">
+    <PageHomeHero />
+  </AppPage>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import AppPage from '@/components/AppPage.vue'
+import PageHomeHero from '@/components/PageHomeHero.vue'
 
 export default Vue.extend({
   components: {
-    AppPage
+    AppPage,
+    PageHomeHero
   },
 
   head: {
     title: 'Vue.js 日本ユーザーグループ',
-    titleTemplate: '%s'
+    titleTemplate: '%s',
+    meta: [
+      { hid: 'description', name: 'description', content: 'Vue.js 日本ユーザーグループは、年に一度の大規模カンファレンス“Vue Fes Japan”の運営や、Vue.js公式ドキュメントの日本語翻訳をはじめ、日本におけるVue.jsの普及と、Vue.jsエコシステムへの貢献を行っている日本最大のVue.jsコミュニティです。' }
+    ]
   }
 })
 </script>
