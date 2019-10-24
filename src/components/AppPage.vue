43d7333afdcae3837929b9549bb09ac352382dfc
kohou <hoh_ko@ricoh-japan.co.jp>
2019-10-24 15:25:53 +0900

    fix jetbrains IDE compile error

--- src/components/AppPage.vue
+++ src/components/AppPage.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="AppPage">
-    <slot />
+    <slot/>
   </div>
 </template>
 
-<style lang="postcss" scoped>
-.AppPage.page-leave-active {
-  transition: opacity 0.25s, transform 0.25s;
-}
+<script lang="ts">
+    import Vue from 'vue'
 
-.AppPage.page-leave-to {
-  opacity: 0;
-  transform: translateY(-4px);
-}
+    export default Vue.extend({})
+
+</script>
+
+<style lang="postcss" scoped>
+  .AppPage.page-leave-active {
+    transition: opacity 0.25s, transform 0.25s;
+  }
+
+  .AppPage.page-leave-to {
+    opacity: 0;
+    transform: translateY(-4px);
+  }
 </style>
