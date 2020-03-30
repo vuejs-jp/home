f0e3767f31e364f0df658550e56d2d8ccead3bb8
Kia Ishii <kia.king.08@gmail.com>
2020-03-30 23:10:52 +0900

    fix: another typo for open graph

--- src/plugins/application.ts
+++ src/plugins/application.ts
@@ -1,5 +1,5 @@
 import Vue from 'vue'
 
 Vue.prototype.$url = function (this: Vue): string {
-  return `https://vuejs.org${this.$route.fullPath}`
+  return `https://vuejs-jp.org${this.$route.fullPath}`
 }
