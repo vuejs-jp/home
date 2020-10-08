1d18fe0d41d299145105a243bae9903f1904cf85
houko <kohou@ctw-inc.net>
2020-10-08 10:51:41 +0900

    チームJSON　DATAを直す

--- src/data/about_team_list.ts
+++ src/data/about_team_list.ts
@@ -4,7 +4,7 @@ export default [
     icon: 'https://avatars0.githubusercontent.com/u/72989?s=400&u=d333c3048e3d6f8ed2a476a3564dba1fa5288b86&v=4',
     name: 'Kazuya Kawauchi',
     workFor: 'Engineer at <a href="https://plaid.co.jp/" style="color: #60af89" target="_blank">PLAID,INC.</a>',
-    desc: '<div>Vue.js日本ユーザーグループ代表。</div><div><a style="color: #60af89" href="https://github.com/kazupon/vue-i18n" target="_blank">vue-i18n</a>の開発者。</div>',
+    desc: '<div>Vue.js日本ユーザーグループ代表。</div><div><a style="color: #60af89" href="https://github.com/kazupon/vue-i18n" target="_blank">vue-i18n</a>の作者。</div>',
     twitter: 'https://twitter.com/kazu_pon',
     github: 'https://github.com/kazupon'
   }
