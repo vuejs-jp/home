c12447927b0ecee5a0dbb460120101e8b1fd87a2
kazuya kawaguchi <kawakazu80@gmail.com>
2022-07-29 00:19:21 +0900

    tweak

--- plugins/router.js
+++ plugins/router.js
@@ -2,13 +2,4 @@ export default ({ app }) => {
   app.nuxt.defaultTransition.beforeEnter = () => {
     app.i18n.finalizePendingLocaleChange()
   }
-       
-  // Optional: wait for locale before scrolling for a smoother transition
-  // app.router.options.scrollBehavior = async (to, from, savedPosition) => {
-  //   // Make sure the route has changed
-  //   if (to.name !== from.name) {
-  //     await app.i18n.waitForPendingLocaleChange()
-  //   }
-  //   return savedPosition || { x: 0, y: 0 }
-  // }
 }
