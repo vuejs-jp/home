19ab6bcf3d4fb39481aa54ecee0b42062502d1d6
Ubugeeei <ubuge1122@gmail.com>
2024-08-10 18:35:16 +0900

    chore: lint setting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,10 @@
 import pluginVueA11y from "eslint-plugin-vuejs-accessibility";
 import withNuxt from "./.nuxt/eslint.config.mjs";
 
-export default withNuxt({
-  ignores: ["_legacy/*"]
-}).append(
-  ...pluginVueA11y.configs["flat/recommended"]
-);
+export default withNuxt({ ignores: ["_legacy/*"] })
+  .override(
+    "nuxt/vue/rules",
+    { rules: { "vue/multi-word-component-names": "off" } }
+  ).append(
+    ...pluginVueA11y.configs["flat/recommended"]
+  );
