0279909bfdf5406262e94abb3d4a2ccc9946febb
Ubugeeei <ubuge1122@gmail.com>
2024-08-10 18:03:28 +0900

    deps: install eslint-plugin-vuejs-accessibility

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,8 @@
+import pluginVueA11y from "eslint-plugin-vuejs-accessibility";
 import withNuxt from "./.nuxt/eslint.config.mjs";
 
 export default withNuxt({
   ignores: ["_legacy/*"]
-});
+}).append(
+  ...pluginVueA11y.configs["flat/recommended"]
+);
