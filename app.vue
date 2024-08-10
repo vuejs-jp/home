efc9c43c8c6031c99416f952a5bf62d29247e8a7
Ubugeeei <ubuge1122@gmail.com>
2024-08-10 18:13:07 +0900

    cfg: disable auto imports

--- app.vue
+++ app.vue
@@ -1,6 +1,12 @@
+<script setup lang="ts">
+import { NuxtRouteAnnouncer, NuxtPage, NuxtLayout } from "#components";
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+  imports: { autoImport: false },
   modules: ["@nuxt/eslint"],
   eslint: {
     config: {
