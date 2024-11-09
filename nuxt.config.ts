9eb00f07732803c0bfaa634c5c2f11a2868e018d
ubugeeei <71201308+ubugeeei@users.noreply.github.com>
2024-11-09 17:55:22 +0900

    feat: unocss configuration (#110)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/eslint"],
+  modules: ["@nuxt/eslint", "@unocss/nuxt"],
   devtools: { enabled: true },
   future: { compatibilityVersion: 4 },
   compatibilityDate: "2024-04-03",
   eslint: {
     config: { standalone: false },
   },
+  unocss: { nuxtLayers: true },
 });
