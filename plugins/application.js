21897b87909bd39d7d2c5e459744b36f405a8e93
Ubugeeei <ubuge1122@gmail.com>
2024-11-09 17:21:14 +0900

    cleanup project

--- plugins/application.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-
-// Vue.prototype.$url = function (this: Vue): string {
-//   return `https://vuejs-jp.org${this.$route.fullPath}`
-// }
-
-Vue.prototype.$url = function () {
-  return `https://vuejs-jp.org${this.$route.fullPath}`
-}
--- plugins/router.js
+++
@@ -1,5 +0,0 @@
-export default ({ app }) => {
-  app.nuxt.defaultTransition.beforeEnter = () => {
-    app.i18n.finalizePendingLocaleChange()
-  }
-}
